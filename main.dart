6d59ee781c19c8bf72bbf6b4ae9b3c3603ae4d16
calvinarieri <ariericalvin@gmail.com>
2024-05-28 00:35:56 +0300

    my first dart class

--- main.dart
+++ main.dart
@@ -1,3 +1,29 @@
 void main(){
-  print('Hello world');
+   print('Hello world');  
+   print(addTwoNumbers(1, 2));
+  dynamic car  =  Car();
+  car.addCarDetails('Alto', 20000, 'Kenya');
+  print(car.carDescription());
+}
+
+int addTwoNumbers(num1,  num2){
+
+  return num1 + num2 ;
+}
+
+class Car{
+  String name = '';
+  int price = 0;
+  String origin = '';
+
+  dynamic carDescription(){
+    return this.name +' ' +this.price.toString() +' '+ this.origin;
+  }
+
+  dynamic addCarDetails(name, price, origin){
+    this.name = name;
+    this.price = price;
+    this.origin = origin;
+  }
+
 }
